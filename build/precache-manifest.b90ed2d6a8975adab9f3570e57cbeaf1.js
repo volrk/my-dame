608e4e7477b6ab66c2cbf7c42241d340a78ca461
volrk <thibaut.boulard@hotmail.fr>
2020-12-05 19:29:02 +0100

    add build

---
+++ build/precache-manifest.b90ed2d6a8975adab9f3570e57cbeaf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6741231671664128edbfec247d63902c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b90835b8804d858585",
+    "url": "/static/css/main.c6fbffd7.chunk.css"
+  },
+  {
+    "revision": "4850efdcd7dd18e59911",
+    "url": "/static/js/2.ddd993fa.chunk.js"
+  },
+  {
+    "revision": "c4b90835b8804d858585",
+    "url": "/static/js/main.c1ca80f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
